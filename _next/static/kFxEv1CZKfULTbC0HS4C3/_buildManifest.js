2390109a29bc032caf26cbb8fd9868ebc7ef6d26
jekatigr <jekatigr@users.noreply.github.com>
2021-11-16 12:48:38 +0000

    deploy: 1c35ae2d0ebf6f2ccbb2f0ed04e7a59c1e95810c

--- _next/static/kFxEv1CZKfULTbC0HS4C3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/742-a04d36030eaa52ea.js","static/chunks/pages/index-b09daad9c905acc7.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
